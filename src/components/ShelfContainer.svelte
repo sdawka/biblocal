ea8c9a3ec5af66c5ce2ef7518bae60adb77cedda
sahil <37510491+sdawka@users.noreply.github.com>
2026-05-10 13:44:30 -0400

    feat: conditional render EmptyShelfIsland when shelf empty

---
+++ src/components/ShelfContainer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { shelf } from '../stores/shelf';
+  import ShelfIsland from './ShelfIsland.svelte';
+  import EmptyShelfIsland from './EmptyShelfIsland.svelte';
+
+  let isEmpty = $state(true);
+
+  $effect(() => {
+    const unsub = shelf.subscribe((s) => {
+      isEmpty = Object.keys(s).length === 0;
+    });
+    return unsub;
+  });
+</script>
+
+{#if isEmpty}
+  <EmptyShelfIsland />
+{:else}
+  <ShelfIsland />
+{/if}
